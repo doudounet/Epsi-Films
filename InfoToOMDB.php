605bdc118457afa2704a31c74d75037d07e3d51e
Ludovic Mantovani <ludovic.mantov@gmail.com>
2015-04-03 11:58:34 +0200

    Update infotoOMDB

--- InfoToOMDB.php
+++ InfoToOMDB.php
@@ -14,7 +14,8 @@ $year = "2014";
 $title = urlencode($title);
 
 //Call the omdb api
-$json=file_get_contents("http://www.omdbapi.com/?t=$title&y=$year");
+//$json=file_get_contents("http://www.omdbapi.com/?t=$title&y=$year");
+$json=file_get_contents("http://www.omdbapi.com/?t=$title");
 
 $details=json_decode($json);
 
@@ -22,25 +23,30 @@ $details=json_decode($json);
 if($details->Response=='True')
 {
     //Print the movie information
-    echo "IMDB-ID : ".$details->imdbID.'<br>';
-    echo "Title : ".$details->Title.'<br>';
-    echo "Year : ".$details->Year.'<br>';
-    echo "Rated : ".$details->Rated.'<br>';
-    echo "Poster Image Path: ".$details->Poster.'<br>';
-    echo "<img src=\"$details->Poster\"><br>";
-    echo "Released Date: ".$details->Released.'<br>';
-    echo "Runtime : ".$details->Runtime.'<br>';
-    echo "Genre : ".$details->Genre.'<br>';
-    echo "Director : ".$details->Director.'<br>';
-    echo "Writer : ".$details->Writer.'<br>';
-    echo "Actors : ".$details->Actors.'<br>';
-    echo "Plot : ".$details->Plot.'<br>';
-    echo "Language : ".$details->Language.'<br>';
-    echo "Country : ".$details->Country.'<br>';
-    echo "Awards : ".$details->Awards.'<br>';
-    echo "Metascore : ".$details->Metascore.'<br>';
-    echo "IMDB Rating : ".$details->imdbRating.'<br>';
-    echo "IMDB Votes : ".$details->imdbVotes.'<br>';
+    //echo "IMDB-ID : ".$details->imdbID.'<br>';
+    //echo "Title : ".$details->Title.'<br>';
+    //echo "Year : ".$details->Year.'<br>';
+    //echo "Rated : ".$details->Rated.'<br>';
+    //echo "Poster Image Path: ".$details->Poster.'<br>';
+    //echo "<img src=\"$details->Poster\"><br>";
+    //echo "Released Date: ".$details->Released.'<br>';
+    //echo "Runtime : ".$details->Runtime.'<br>';
+    //echo "Genre : ".$details->Genre.'<br>';
+    //echo "Director : ".$details->Director.'<br>';
+    //echo "Writer : ".$details->Writer.'<br>';
+    //echo "Actors : ".$details->Actors.'<br>';
+    //echo "Plot : ".$details->Plot.'<br>';
+    //echo "Language : ".$details->Language.'<br>';
+    //echo "Country : ".$details->Country.'<br>';
+    //echo "Awards : ".$details->Awards.'<br>';
+    //echo "Metascore : ".$details->Metascore.'<br>';
+    //echo "IMDB Rating : ".$details->imdbRating.'<br>';
+    //echo "IMDB Votes : ".$details->imdbVotes.'<br>';
+    foreach ($details as $key => $value)
+    {
+        echo $key.' : '.$value.'<br />';
+    }
+    //var_dump($details);
 }
 //Show message if the movie information is not returned by APIs
 else
